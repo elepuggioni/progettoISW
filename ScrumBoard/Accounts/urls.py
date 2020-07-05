49678c42a3b16c7a8895c600b03bd462641edf4e
alesatta <a.satta15@studenti.unica.it>
2020-07-05 15:57:27 +0200

    conclusi registrazione e login

--- ScrumBoard/Accounts/urls.py
+++ ScrumBoard/Accounts/urls.py
@@ -4,7 +4,7 @@ from ScrumBoard import views
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name = 'home'),
     path('profilo/', views.profilo),
     path('login/', views.loginPage, name = 'login'),
     path('register/', views.registerPage, name = 'register'),
--- ScrumBoard/Accounts/views.py
+++ ScrumBoard/Accounts/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from ScrumBoard import *
 from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 from .models import *
@@ -14,10 +17,23 @@ def registerPage(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for' + user )
+            return redirect('login')
+
     context = {'form': form}
     return render(request, 'register.html', context)
 
 def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
     context = {}
     return render(request, 'login.html', context)
 
